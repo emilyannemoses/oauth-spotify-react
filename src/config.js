bf153f12b2c811941f4c7aab2cf3075abac8fb63
eamoses <e.a.moses@gmail.com>
2019-07-01 17:26:33 -0500

    saving

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = "279710054a5e456ca6a80d4561e56868";
+export const clientId = "<YOUR CLIENTID>";
 export const redirectUri = "http://localhost:3000/callback";
 export const scopes = [
     "user-read-private",
